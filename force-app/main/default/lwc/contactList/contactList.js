724f92ce177d2c4fd2385f883d86f21362d4d18d
manalijadhav97 <manalijadhav97@gmail.com>
2023-07-04 22:07:48 +0200

    adding components

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,32 @@
+import { LightningElement,wire } from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+export default class ContactList extends LightningElement {
+    
+    columns = [
+        { label: 'First Name', fieldName: 'firstName', hideDefaultActions : true },
+        { label: 'Last Name', fieldName: 'lastName', hideDefaultActions : true  },
+        { label: 'Email', fieldName: 'email', type: 'email', hideDefaultActions : true },
+    ];
+
+    contactInfo = [];
+    error;
+
+    @wire(getContacts)
+    wiredContacts({data , error}){
+        if (error) {
+            this.contactInfo = undefined;
+            if (Array.isArray(error.body)) {
+                this.error = error.body.map(e => e.message).join(', ');
+            } else if (typeof error.body.message === 'string') {
+                this.error = error.body.message;
+            }
+        } else if (data) {
+            this.contactInfo = [...data].map(ele => {
+                return {id: ele.Id, firstName : ele.FirstName, lastName: ele.LastName, email:ele.Email}
+            });
+            this.error = undefined;
+        }
+    }
+    
+}
